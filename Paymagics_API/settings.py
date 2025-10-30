a1b543c1fdc5816727f2660b2d137d2e8cc82296
Sreejith S <sreejith.s.official9@gmail.com>
2025-10-30 09:44:56 +0530

    testing

--- Paymagics_API/settings.py
+++ Paymagics_API/settings.py
@@ -68,6 +68,9 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite default
     "http://127.0.0.1:5173",
+    "http://localhost:3001",
+    "http://localhost:3000",
+    "https://paymagics-frontend.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
