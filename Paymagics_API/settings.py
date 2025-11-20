a2b2a406de0fe54684faf620ed57c2939bd22933
Sreejith S <sreejith.s.official9@gmail.com>
2025-11-20 14:20:41 +0530

    Celery setup changed

--- Paymagics_API/settings.py
+++ Paymagics_API/settings.py
@@ -64,11 +64,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Vite default
-    "http://127.0.0.1:5173",
-    "http://localhost:3001",
-    "http://localhost:3000",
-    "https://paymagics-frontend.vercel.app",
+    '*'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
